270efb5f8ea2e62181985325ee9cb926fee44646
Anuj Kundu <anuj.skundu41227@gmail.com>
2026-06-11 20:49:31 +0530

    Fix EKS deployment and state synchronization

--- eks/providers.tf
+++ eks/providers.tf
@@ -35,13 +35,6 @@ provider "aws" {
 
 # The kubernetes provider must be configured dynamically using the
 # cluster endpoint and certificate from the EKS module outputs.
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+ provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
